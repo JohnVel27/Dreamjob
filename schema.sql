a351461d506bf072daccabb36f5e04169818c4c1
JohnVel27 <124770839+JohnVel27@users.noreply.github.com>
2024-10-16 17:23:15 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE JOBDREAMERS ( 
-    JobDreamerID INT PRIMARY KEY AUTO_INCREMENT, -- Primary key with auto-increment for unique JOBDREAMERS IDs 
-    Firstname TEXT NOT NULL, -- Name of the Firstname (cannot be null) 
-    Lastname TEXT NOT NULL, -- Name of the Lastname (cannot be null) 
-    Email TEXT NOT NULL,-- Name of the Email (cannot be null) 
-    Gender TEXT NOT NULL,-- Name of the Gender (cannot be null) 
-    DreamJob TEXT NOT NULL,-- Name of the DreamJob (cannot be null) 
-    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the transaction record was added 
+CREATE TABLE jobdreamers (
+    jobdreamerID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    dreamjob VARCHAR(100) NOT NULL,
+    dateadded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
